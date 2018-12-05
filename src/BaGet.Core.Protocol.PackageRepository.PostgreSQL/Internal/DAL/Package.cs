62c288df3da902367ac19b96b7194b1736f621de
vitaliy-orlov <vitaliuorlov@gmail.com>
2018-12-05 23:48:21 +0300

    fix columns

--- src/BaGet.Core.Protocol.PackageRepository.PostgreSQL/Internal/DAL/Package.cs
+++ src/BaGet.Core.Protocol.PackageRepository.PostgreSQL/Internal/DAL/Package.cs
@@ -4,7 +4,7 @@ namespace BaGet.Core.Protocol.Repository.PostgreSQL.Internal.DAL
 {
     internal class Package
     {
-        [Column("id"), PrimaryKey]      public string Id            { get; set; } // varchar(128)
-        [Column("version"), PrimaryKey] public string VersionString { get; set; } // varchar(64)
+        [Column("id"), PrimaryKey   ] public string Id            { get; set; } // varchar(128)
+        [Column("version"), NotNull ] public string VersionString { get; set; } // varchar(64)
     }
 }
